dc556c781df89ed3ceae73b11c8ba0f03efee497
CongMinh26 <congminhad1999@gmail.com>
2022-05-03 16:37:55 +0700

    Navigation
    code navigation

--- my-app/src/components/header/Header.tsx
+++ my-app/src/components/header/Header.tsx
@@ -7,12 +7,12 @@ export const Header = () => {
   return (
     <header className='container header__container'>
       <h5>Hello I'm</h5>
-      <h1>Cong Minh</h1>
-      <h5 className='text-light'>Fullstack Developer</h5>
+      <h1>Đạm Đài Nguyệt</h1>
+      <h5 className='text-light'>Hoa Thần Cung</h5>
       <CTA />
       <HeaderSocials />
       <div className="me">
-        <img src={require('../../assets/me.png')} alt="me" />
+        <img src={require('../../assets/DamDai.png')} alt="me" />
       </div>
       <a href='#contact' className="scroll__down">Scroll Down</a>
     </header>
--- my-app/src/components/nav/Nav.tsx
+++ my-app/src/components/nav/Nav.tsx
@@ -1,7 +1,26 @@
 import React from 'react'
-
-export const Nav = () => {
+import { AiOutlineHome, AiOutlineUser } from 'react-icons/ai'
+import { RiServiceLine } from 'react-icons/ri'
+import { BiBook, BiMessageSquareDetail } from 'react-icons/bi'
+import './nav.css'
+import { useState } from 'react'
+const Nav = () => {
+  const [active, setActive] = useState('#')
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#"
+        onClick={() => setActive('#')} className={active === '#' ? 'active' : ''}><AiOutlineHome /></a>
+      <a href="#about"
+        onClick={() => setActive('#about')} className={active === '#about' ? 'active' : ''}><AiOutlineUser /></a>
+      <a href="#experience"
+        onClick={() => setActive('#experience')} className={active === '#experience' ? 'active' : ''}><BiBook /></a>
+      <a href="#services"
+        onClick={() => setActive('#services')} className={active === '#services' ? 'active' : ''}><RiServiceLine /></a>
+      <a href="#contact"
+        onClick={() => setActive('#contact')} className={active === '#contact' ? 'active' : ''}><BiMessageSquareDetail /></a>
+    </nav>
   )
 }
+export default Nav
+
+
